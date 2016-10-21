083af008dd00aea7d9e034350a1bd0a22a20b60c
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2016-10-21 19:31:41 +0100

    Initial commit of the cloud native .NET books showcase.

---
+++ MicroserviceApp.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace QAware.OSS
+{
+    public class MicroserviceApp
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+             .UseKestrel()
+             .UseStartup<Startup>()
+             .Build();
+
+            host.Run();
+        }
+    }
+
+    class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IBooksRepository, MemoryBooksRepository>();
+            services.AddMvc();
+        }
+    }
+}
